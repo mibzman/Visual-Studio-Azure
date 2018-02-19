ddc918b754da59a406af77fa26a7a738fa572a9c
mibzman <sam@hungerperks.com>
2018-02-19 10:52:51 -0500

    first

---
+++ test-web.Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace testweb.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestCase()
+        {
+        }
+    }
+}
